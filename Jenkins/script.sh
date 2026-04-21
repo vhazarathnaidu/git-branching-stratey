c8ee74ad28c90fc2142a0484dd9a0f2468c185ae
sahithya <t.k.sahithya2003@gmail.com>
2026-04-21 12:27:37 +0530

    Added Jebkins script file

---
+++ Jenkins/script.sh
@@ -0,0 +1,27 @@
+sudo apt update
+
+sudo spt install open jdk-21-jre -y
+
+sudo wget  -O/etc/apt/keyrings/jenkins-keyring.asc\
+https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+
+echo "deb[signed-by=/etc/apt/keyrings/jenkins-keyring.asc]"\
+https://pkg.jenkins.io/debian-stable binary | sudo tee \
+/etc/apt/sources.list.d/jenkins.list>/dev/null
+
+sudo apt update 
+
+sudo apt install jenkins -y
+
+sudo systemctl start jenkins
+
+sudo systemctl enable jenkins 
+
+sudo systemctl status jnekins
+
+http://your-ip:8080
+ 
+sudo cat/var/lib/jenkins/secrets/initialAdminPassword
+
+
+
