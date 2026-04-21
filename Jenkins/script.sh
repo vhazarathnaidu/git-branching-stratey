f7411f20e583b0300cb6176230459622854d70ae
sahithya <t.k.sahithya2003@gmail.com>
2026-04-21 18:01:04 +0530

    added the new line

--- Jenkins/script.sh
+++ Jenkins/script.sh
@@ -1,24 +1,31 @@
-sudo apt update
+#!/bin/bash
 
-sudo apt install open jdk-21-jre -y
+# Update system
+sudo apt update -y
 
-sudo wget  -O/etc/apt/keyrings/jenkins-keyring.asc\
-https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+# Install Java (Jenkins requires Java)
+sudo apt install openjdk-21-jre -y
 
-echo "deb[signed-by=/etc/apt/keyrings/jenkins-keyring.asc]"\
-https://pkg.jenkins.io/debian-stable binary | sudo tee \
-/etc/apt/sources.list.d/jenkins.list>/dev/null
+# Create keyrings directory
+sudo mkdir -p /etc/apt/keyrings
 
-sudo apt update 
+# Download Jenkins key
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /etc/apt/keyrings/jenkins-keyring.asc > /dev/null
 
+# Add Jenkins repository
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Update repo
+sudo apt update -y
+
+# Install Jenkins
 sudo apt install jenkins -y
 
+# Start Jenkins
 sudo systemctl start jenkins
 
-sudo systemctl enable jenkins 
+# Enable Jenkins at boot
+sudo systemctl enable jenkins
 
-sudo systemctl status jnekins
-
-
-
-
+# Check status
+sudo systemctl status jenkins
